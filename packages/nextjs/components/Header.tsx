25cc75b398f3f208ef08c3857f04447b79c0e68c
Victor <victoraranguren.dev@gmail.com>
2024-09-05 22:49:26 -0400

    Navbar modified

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -20,6 +20,14 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/",
   },
   {
+    label: "Create",
+    href: "/create",
+  },
+  {
+    label: "Search",
+    href: "/search",
+  },
+  {
     label: "Debug Contracts",
     href: "/debug",
     icon: <BugAntIcon className="h-4 w-4" />,
